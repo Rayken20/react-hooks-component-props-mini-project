a70d76c202f541167b03f4f700a7bf0cdda44618
Rachael <rachael92njoki@gmail.com>
2024-02-03 09:01:39 +0300

    complete

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-const Article = ({ title, date = 'January 1, 1970', preview }) => {
+const Article = ({ title, date = 'January 1, 1970', preview, minutes }) => {
+  const renderReadingTime = (time) => {
+    const coffeeCups = Math.ceil(time / 5);
+    const bentoBoxes = Math.ceil(time / 10);
+
+    if (time < 30) {
+      return `☕️`.repeat(coffeeCups) + ` ${time} min read`;
+    } else {
+      return `🍱`.repeat(bentoBoxes) + ` ${time} min read`;
+    }
+  };
+
   return (
     <article>
       <h3>{title}</h3>
       <small>Date: {date}</small>
       <p>{preview}</p>
+      <p>{renderReadingTime(minutes)}</p>
     </article>
   );
 };
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -5,7 +5,7 @@ const ArticleList = ({ posts }) => {
   return (
     <main>
       {posts.map(post => (
-        <Article key={post.id} title={post.title} date={post.date} preview={post.preview} />
+        <Article key={post.id} title={post.title} date={post.date} preview={post.preview} minutes={post.minutes} />
       ))}
     </main>
   );
